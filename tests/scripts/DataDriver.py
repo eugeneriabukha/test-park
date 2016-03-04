5e8c6757750a0aeeda16d33619d6271ddab7c5b6
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-03-04 15:12:04 -0700

    changes4

--- tests/scripts/DataDriver.py
+++ tests/scripts/DataDriver.py
@@ -76,7 +76,7 @@ class DataDriver(dict):
 
         # find the current path of the data file
         #sPath = str(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))+"/data/"
-        sPath = str(os.path.dirname(os.path.abspath(__file__)))
+        sPath = str(os.path.dirname(os.path.abspath(__file__).parent))
 
         sPath = sPath + "/data/"
         sPath = sPath + str(fileName)
