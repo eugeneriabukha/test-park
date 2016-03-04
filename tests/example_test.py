6868887f81e6d703af9c2ec8f7247c8ac6c72c0b
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-03-04 14:36:56 -0700

    could not find the folders so moved them again

--- tests/example_test.py
+++ tests/example_test.py
@@ -12,11 +12,9 @@ def test_that_live_tv_is_playing():
     stbt.press('KEY_CLOSE')  # Close any open menus
     assert stbt.wait_for_motion()
 
-
 def test_that_stb_tester_logo_is_shown():
     stbt.press('KEY_CHANNELUP')
     assert stbt.wait_for_match('stb-tester-logo.png')
-
 
 def test_read_menu():
     stbt.press('KEY_CLOSE')
