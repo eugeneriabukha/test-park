ca1beda676f9e4a103ff4cb846b4b95ff71a8545
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-03-11 15:39:41 -0700

    understand ocr 14

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -130,9 +130,9 @@ class Search:
         # TODO: to get output information about the current netflix level and set it in the flag bFlag
         # TODO: make use of the variables
         a = stbt.Region(800,100,width=500,height=500)
-        oResults = stbt.match_text("Netflix",None,a)
+        oResults = stbt.match_text("Net",None,a)
         print oResults.match
-        #print stbt.ocr(None,a)
+        print stbt.ocr(None,a)
 
 
         bFlag = False
