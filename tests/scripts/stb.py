23a02c8ea68cb08ff2fd88deab7d2cdd08fc380c
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-03-16 14:19:25 -0600

    switched couple of constants

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -28,7 +28,11 @@ import time
 #=============================================================================#
 
 # COMMON CONSTANTS
-DEFAULT_SEARCH_CHAR = 'P'
+DEFAULT_SEARCH_CHAR = "P"
+SEARCH_NAVIGATION_SUCCESS = "Navigation To Search Screen: Successful"
+SEARCH_NAVIGATION_FAILURE = "Navigation Failure: Unable to navigate to Search screen"
+SEARCH_CORRECT_NETFLIX_SETTING = "The existing setting of Netflix is correct. No further changes"
+SEARCH_KEYSTROKES = ['KEY_EPG','KEY_MENU','KEY_DOWN','KEY_SELECT']
 
 #=============================================================================#
 # Class: Navigate
@@ -63,26 +67,25 @@ class Navigate:
     # Usage Examples:
     #=============================================================================#
     def Search(self):
-        # press search button
-        stbt.press('KEY_EPG')
-        # press menu button
-        stbt.press('KEY_MENU')
-        stbt.press('KEY_DOWN')
-        # get down to select search
-        stbt.press('KEY_SELECT')
+        global global_wait
+
+        # press keystrokes for search
+        for sKeyStroke in SEARCH_KEYSTROKES:
+            stbt.press(sKeyStroke)
+            time.sleep(global_wait)
 
         # this checks if we are on the right screen, and updates actual result
         # check for presence of Search Logo
         time.sleep(Constants.LONG_WAIT)
         oSearchLogo = stbt.match("../images/Search_Logo.png")
-        oSearchLogo.match=True
+        oSearchLogo.match = True
         # if the search page do not exist, then exit the test case
         if oSearchLogo.match == True:
             self.instruction.actualresult = self.instruction.expectedresult
-            print "Navigation To Search Screen: Successful"
+            print SEARCH_NAVIGATION_SUCCESS
         else:
             self.instruction.actualresult = Constants.STATUS_NAVIGATION_FAILURE
-            print "Navigation Failure: Unable to navigate to Search screen"
+            print SEARCH_NAVIGATION_FAILURE
 
 #=============================================================================#
 # Class: Search
@@ -109,7 +112,8 @@ class Search:
     #=============================================================================#
     def __init__(self,oInstruction):
         self.instruction = oInstruction
-        self.ResultMatrix=[]
+        self.ResultMatrix = []
+
     #=============================================================================#
     # Method:
     # Description:
@@ -118,74 +122,99 @@ class Search:
     #=============================================================================#
     def getResults(self):
             resultsRegion = stbt.Region(x=400, y=100, width=600, height=700)
-            resultsString= stbt.ocr(region=resultsRegion, tesseract_user_words=['MOVIE','TV','SPORTS','PERSON','0']) 
-            resultsString=resultsString.strip()
-            resultsString=list(resultsString)
-            resultsString[0]='0'
-            resultsString=''.join(resultsString)
-            resultsString=resultsString.splitlines()
+            resultsString = stbt.ocr(region=resultsRegion, tesseract_user_words=['MOVIE','TV','SPORTS','PERSON','0']) 
+            resultsString = resultsString.strip()
+            resultsString = list(resultsString)
+            resultsString[0] = '0'
+            resultsString = ''.join(resultsString)
+            resultsString =resultsString.splitlines()
             resultsString = [line.strip() for line in resultsString if line.strip()]
-            indexTV=-1
-            indexMovie=-1
-            indexSports=-1
-            indexPeople=-1
+            indexTV = -1
+            indexMovie = -1
+            indexSports = -1
+            indexPeople = -1
             if 'TV' in resultsString:
-                indexTV= resultsString.index('TV')
+                indexTV = resultsString.index('TV')
             if 'MOVIE' in resultsString:
-                indexMovie= resultsString.index('MOVIE')
+                indexMovie = resultsString.index('MOVIE')
             if 'SPORTS' in resultsString:
-                indexSports=resultsString.index('SPORTS')
+                indexSports = resultsString.index('SPORTS')
             if 'PERSON' in resultsString:
-                indexPeople= resultsString.index('PERSON')
-            listResults=resultsString[:1]
-            listTV=[]
-            listMovie=[]
-            listPeople=[]
-            listSports=[]
-            if(indexTV!=-1):
-                if(indexMovie!=-1):
-                    listTV=resultsString[indexTV+1:indexMovie]
-                elif(indexSports!=-1):
-                    listTV=resultsString[indexTV+1:indexSports]
-                elif(indexPeople !=-1):
-                    listTV=resultsString[indexTV+1:indexPeople]
+                indexPeople = resultsString.index('PERSON')
+            listResults = resultsString[:1]
+            listTV = []
+            listMovie = []
+            listPeople = []
+            listSports = []
+            if(indexTV != -1):
+                if(indexMovie != -1):
+                    listTV = resultsString[indexTV+1:indexMovie]
+                elif(indexSports != -1):
+                    listTV = resultsString[indexTV+1:indexSports]
+                elif(indexPeople != -1):
+                    listTV = resultsString[indexTV+1:indexPeople]
                 else:
-                    listTV=resultsString[indexTV+1:]
-            if(indexMovie!=-1):
-                if(indexSports!=-1):
-                    listMovie=resultsString[indexMovie+1:indexSports]
-                elif(indexPeople !=-1):
-                    listMovie=resultsString[indexMovie+1:indexPeople]
+                    listTV = resultsString[indexTV+1:]
+            if(indexMovie != -1):
+                if(indexSports != -1):
+                    listMovie = resultsString[indexMovie+1:indexSports]
+                elif(indexPeople != -1):
+                    listMovie = resultsString[indexMovie+1:indexPeople]
                 else:
-                    listMovie=resultsString[indexMovie+1:]
-            if(indexSports !=-1):
-                if(indexPeople !=-1):
-                    listSports=resultsString[indexSports+1:indexPeople]
+                    listMovie = resultsString[indexMovie+1:]
+            if(indexSports != -1):
+                if(indexPeople != -1):
+                    listSports = resultsString[indexSports+1:indexPeople]
                 else:
-                    listSports=resultsString[indexSports+1:]
-            if(indexPeople!=-1):
-                listPeople=resultsString[indexPeople+1:]
+                    listSports = resultsString[indexSports+1:]
+            if(indexPeople != -1):
+                listPeople = resultsString[indexPeople+1:]
 
-            self.ResultMatrix=[]
-            dictResults={'Firstline':listResults,'TV':listTV,'Movie':listMovie,'Person':listPeople,'Sports':listSports}
+            self.ResultMatrix = []
+            dictResults = {'Firstline':listResults,'TV':listTV,'Movie':listMovie,'Person':listPeople,'Sports':listSports}
             for key in dictResults.keys():
                 for value in dictResults[key]:
-                    index=value.split(' ',1)[0]
-                    title=value.split(' ',1)[1]
+                    index = value.split(' ',1)[0]
+                    title = value.split(' ',1)[1]
                     self.ResultMatrix.append((key,index,title))
             time.sleep(Constants.LONG_WAIT)
             return self.ResultMatrix
 
+    #=============================================================================#
+    # Method:
+    # Description:
+    # Returns: NA
+    # Usage Examples:
+    #=============================================================================#
     def SearchResultbyIndex(self,index):  
         return [item for item in self.ResultMatrix if item[1] == index]     
 
+    #=============================================================================#
+    # Method:
+    # Description:
+    # Returns: NA
+    # Usage Examples:
+    #=============================================================================#
     def SearchResultsbyType(self,typevideo):
         return [item for item in self.ResultMatrix if item[0] == typevideo]
+
+    #=============================================================================#
+    # Method:
+    # Description:
+    # Returns: NA
+    # Usage Examples:
+    #=============================================================================#
     def SearchResultsbyTitle(self,title):
-        return [item for item in self.ResultMatrix if item[2] == title]              
+        return [item for item in self.ResultMatrix if item[2] == title]
+
+    #=============================================================================#
+    # Method:
+    # Description:
+    # Returns: NA
+    # Usage Examples:
+    #=============================================================================#
     def Title(self):
         global global_wait
-
         # fetch data from the instruction
         oTestData = self.instruction.testdata_detailed
         for dValue in oTestData.values():
@@ -198,6 +227,8 @@ class Search:
         for keyStroke in lKeyStrokes:
             stbt.press(keyStroke)
             time.sleep(global_wait)
+        
+        # Needed a short wait before capturing the details from the screen
         time.sleep(Constants.SHORT_WAIT)
 
         bActualNetflixStatus = False
@@ -233,65 +264,6 @@ class Search:
         else:
             self.instruction.actualresult = Constants.STATUS_SEARCH_FAILURE
             print "Search Failure: Error in performing search"
-
-         
-            
-'''
-        textOnScreen = stbt.ocr(region=stbt.Region(x=1000, y=200, width=500, height=600), tesseract_user_words=['Netflix','including','Not','Including']) 
-        if bIncludeNetflix==True:
-            if textOnScreen.find("Including Netflix") != -1:
-                print "The existing setting of Netflix is correct. No further changes"
-            else:
-                print "The existing Netflix settings is NOT correct. Fixing the search results to incorporate Netflix settings"
-                # fixing netflix results
-                stbt.press('KEY_RED')
-                time.sleep(Constants.SHORT_WAIT)
-                stbt.press('KEY_SELECT')
-                time.sleep(Constants.LONG_WAIT)
-        else:
-            if textOnScreen.find("Not including Netflix") != -1:
-                print "The existing setting of Netflix is correct. No further changes"
-            else:
-                print "The existing Netflix settings is NOT correct. Fixing the search results to incorporate Netflix settings"
-                # fixing netflix results
-                stbt.press('KEY_RED')
-                time.sleep(Constants.SHORT_WAIT)
-                stbt.press('KEY_SELECT')
-                time.sleep(Constants.LONG_WAIT)
-
-'''
-
-'''
-        bActualNetflixStatus = False
-        if textOnScreen.find("Including Netflix") != -1:
-            bActualNetflixStatus = True
-        if(bIncludeNetflix == bActualNetflixStatus):
-            self.instruction.actualresult = self.instruction.expectedresult
-            print "Search performed successfully"
-        else:
-'''
-
-
-'''
-        if bIncludeNetflix==True:
-            if textOnScreen.find("Including Netflix") != -1:
-                self.instruction.actualresult = self.instruction.expectedresult
-                print "Search performed successfully"
-            else:
-                self.instruction.actualresult = Constants.STATUS_SEARCH_FAILURE
-                print "Search Failure: Error in performing search"
-        else:
-            if textOnScreen.find("Not including Netflix") != -1:
-                self.instruction.actualresult = self.instruction.expectedresult
-                print "Search performed successfully"
-            else:
-                self.instruction.actualresult = Constants.STATUS_SEARCH_FAILURE
-                print "Search Failure: Error in performing search"
-
-'''
-
-
-
 
 
 #=============================================================================#
