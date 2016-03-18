7d857bd4cbfe2adce02f984b7a957f9d4a1fa1cc
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-03-17 22:41:47 -0600

    calling fetch results

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -172,6 +172,7 @@ class Search:
         else:
             self.instruction.actualresult = Constants.STATUS_SEARCH_FAILURE
             print SEARCH_NEGATIVE
+        self.FetchResults()
 
     def FetchResults(self):
         """
