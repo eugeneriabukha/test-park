1cf1d2043bafffec0e82dd3f070a90ab791614a1
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-03-30 07:47:24 -0600

    movement in the tab1

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -261,7 +261,8 @@ class Navigate:
         lKeyStrokes = []
         # to decide the number of moves to reach the destination, getting abs of difference
         iLastCounter = abs(iDifference)
-        print "iLastCounter " + iLastCounter
+        print "iLastCounter " 
+        print iLastCounter
         for iCounter in range(0,iLastCounter):
             lKeyStrokes.append(sKeyStroke)
 
