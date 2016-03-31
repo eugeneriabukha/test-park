c8fbde2c78987147413a487d44084a9db3f96bb5
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-03-31 11:03:12 -0600

    increasing height and width a little bit

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -96,9 +96,9 @@ REGION_RESULTS = {'x': 490, 'y': 123, 'width': 475, 'height': 590}
 REGION_DIAGNOSTICS_LOGO = {'x': 204, 'y': 58, 'width': 154, 'height': 38}
 REGION_DIAGNOSTICS = {'x': 270, 'y': 447, 'width': 474, 'height': 41}
 REGION_FRANCHISEPAGE = {'x':180,'y': 58, 'width':200, 'height':53}
-REGION_PROGRAM_TITLE = {'x':310,'y': 140, 'width':300, 'height':42}
-REGION_SPORTS_GROUP_TITLE = {'x':286,'y': 120, 'width':300, 'height':42}
-REGION_PERSON_TITLE = {'x':206,'y': 120, 'width':300, 'height':42}
+REGION_PROGRAM_TITLE = {'x':310,'y': 140, 'width':350, 'height':45}
+REGION_SPORTS_GROUP_TITLE = {'x':286,'y': 120, 'width':350, 'height':45}
+REGION_PERSON_TITLE = {'x':206,'y': 120, 'width':350, 'height':45}
 REGION_FRANCHISE_HEADER = {'x':338,'y':42, 'width':648, 'height':69}
 
 DICT_FRANCHISE_TITLE = {
