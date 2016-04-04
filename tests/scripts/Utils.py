3ee5fd82d96c9e8f700c0b7e106fa670926265a1
prithvinathm <prithvi@INSLE0040281-MAC.local>
2016-04-04 13:54:34 -0600

    test logging

--- tests/scripts/Utils.py
+++ tests/scripts/Utils.py
@@ -17,7 +17,7 @@ import logging
 import logging.handlers
 import sys
 
-class cLogger(logging.getLogger()):
+class cLogger(obj):
     def __init__(self,logLevel=None):
         FORMAT = "[%(filename)s:%(lineno)s - %(funcName)s()] %(levelname)s %(message)s"
         logging.basicConfig(level=logLevel,format=FORMAT, stream=sys.stdout)
