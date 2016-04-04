b6f1d8e9e2885fbe21ab7c957a79a5f227685ca1
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-04-04 14:30:38 -0600

    trying to throw an exception

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -608,7 +608,7 @@ class Search:
             self.instruction.actualresult = self.instruction.expectedresult
             Logger.note.info( POPULAR_SEARCH_RESULTS_MATCH)
         else:
-            stbt.get_frame()
+            raise stbt.UITestFailure
             self.instruction.actualresult = Constants.STATUS_FAILURE
             Logger.note.error( POPULAR_SEARCH_RESULTS_FAILURE)
 
