eb78180261a38bf6bc9d6e5fb939acb35cd80dec
prithvinathm <prithvi@INSLE0040281-MAC.local>
2016-04-04 13:20:05 -0600

    test logging

--- tests/scripts/Utils.py
+++ tests/scripts/Utils.py
@@ -20,7 +20,7 @@ import sys
 class cLogger(logging.getLoggerClass()):
     def __init__(self,logLevel=None):
         FORMAT = "[%(filename)s:%(lineno)s - %(funcName)s()] %(levelname)s %(message)s"
-        logging.basicConfig(level=logLevel,format=FORMAT, stream=sys.stdout)
+        self.basicConfig(level=logLevel,format=FORMAT, stream=sys.stdout)
         #self.note = logging.getLogger('stb')
     
 Logger=cLogger(logLevel=logging.DEBUG)
