3a1cd2ce4f58ab47d0c6c91ad33b73c743eb8d6c
prithvinathm <prithvi@INSLE0040281-MAC.local>
2016-04-08 14:54:45 -0600

    Added Debug Messages in Keyword Driver

--- tests/scripts/KeywordDriver.py
+++ tests/scripts/KeywordDriver.py
@@ -365,7 +365,7 @@ class Execution:
         Logger.note.debug('Instruction Dict')
         Logger.note.debug(self.instructionsDict.items())
         Logger.note.debug("Sorted Values in  Instruction Dict")
-        for instruction in sorted(self.instructionsDict.values()):
+        for instruction in sorted(self.instructionsDict.keys()):
             Logger.note.debug(instruction.PrettyPrint())
 
         for oInstruction in sorted(self.instructionsDict.values()):
