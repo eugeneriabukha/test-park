4d36b6598b3c53f37ff0ac55050475b80ebf3ba6
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-04-18 14:35:33 -0600

    correcting a debug statement

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -616,7 +616,7 @@ class Search:
         # Navigate from first counter to last counter
         for iCounter in range(iFirstCounter,iLastCounter):
             sCapturedText = lResults[iCounter]
-            Logger.note.debug("Inside for loop  %d : %d" % (iCounter,sCapturedText))
+            Logger.note.debug("Inside for loop  %d : %d" % (str(iCounter),sCapturedText))
 
 
 
