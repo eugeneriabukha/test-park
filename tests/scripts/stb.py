ef38bd93e03fb99414072bdc3fb2b320f66e2d52
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-04-28 14:56:57 -0600

    look to see if strip is creating an issue

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -1318,17 +1318,17 @@ class Movies:
         oInstruction: an instruction object with keyword, its respected expected result,
         option and its data
     """
-    def VerifyCurrentPage(self):
+    def VerifyCurrentPage(self,sExpectedTitle=None):
         """
         Fetches the current page of the movies
 
         Returns:
             name of current tab which is selected
         """
-        oTestData = self.instruction.testdata_detailed
-        sExpectedTitle = oTestData[Constants.DIRECT_INPUT]
-        sExpectedTitle = sExpectedTitle.strip()
-        Logger.note.debug("past here")
+        if sExpectedTitle == None:
+            oTestData = self.instruction.testdata_detailed
+            sExpectedTitle = oTestData[Constants.DIRECT_INPUT]
+            #sExpectedTitle = sExpectedTitle.strip()
 
         if sExpectedTitle in LIST_MOVIES:
             Logger.note.debug("A valid sub tab within the Movies : [%s]" % sExpectedTitle)
