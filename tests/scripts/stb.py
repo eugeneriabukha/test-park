f9cafea1cb9ee02c1127fdfae3c598e8c51ec32c
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-05-03 15:48:30 -0600

    checking if switching region helps

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -163,8 +163,7 @@ REGION_GUIDEPROGRAM = {'x':990,'y':176, 'width':260, 'height':120}
 REGION_TOPNAV = {'x':374,'y':44, 'width':626, 'height':76}
 REGION_MOVIES_TOPNAV = {'x':32,'y':120, 'width':366, 'height':55}
 REGION_MOVIE_CAROUSEL = {'x':30,'y':150, 'width':160, 'height':500}
-REGION_SHOW_CAROUSEL = {'x':27,'y':150, 'width':250, 'height':500}
-REGION_SHOW_HEADER = {'x':172,'y': 58, 'width':250, 'height':53}
+REGION_SHOW_CAROUSEL = {'x':20,'y':150, 'width':300, 'height':600}
 
 DICT_FRANCHISE_TITLE = {
     TEXT_TV_SHOW : REGION_PROGRAM_TITLE,
