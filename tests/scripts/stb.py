010997a9beb2ea53ebc9335042072d2f2dc214d7
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-05-26 10:06:29 -0600

    longest title may be

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -569,7 +569,7 @@ class Search:
             sTitle = Utils.GetDynamicTitle()
         elif sTitle == Constants.NETFLIX:
             sTitle = Utils.GetDynamicNetflixTitle()
-            sTitle = "Terrace House: Boys & Girls in the City"
+            sTitle = "Trailer Park Boys: Drunk, High and Unemployed: Live In Austin"
 
         # save the searched title for future reference
         Utils.SetSearchedTitle(sTitle)
