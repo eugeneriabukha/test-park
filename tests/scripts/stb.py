ce50cceb71aeff90f5f66385f44390d2df5b8c6d
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-05-26 09:10:04 -0600

    chelsea testing

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -569,7 +569,7 @@ class Search:
             sTitle = Utils.GetDynamicTitle()
         elif sTitle == Constants.NETFLIX:
             sTitle = Utils.GetDynamicNetflixTitle()
-            sTitle = "Chef's Table"
+            sTitle = "Chelsea"
 
         # save the searched title for future reference
         Utils.SetSearchedTitle(sTitle)
