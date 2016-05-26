316e72c76cefab33d6af62690ffe2012ce97c3ec
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-05-26 09:12:52 -0600

    two special characters

--- tests/scripts/stb.py
+++ tests/scripts/stb.py
@@ -569,7 +569,7 @@ class Search:
             sTitle = Utils.GetDynamicTitle()
         elif sTitle == Constants.NETFLIX:
             sTitle = Utils.GetDynamicNetflixTitle()
-            sTitle = "Chelsea"
+            sTitle = "Chris D'Elia: Incorrigible"
 
         # save the searched title for future reference
         Utils.SetSearchedTitle(sTitle)
