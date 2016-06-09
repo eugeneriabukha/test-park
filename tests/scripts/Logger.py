e02ceb71240ac0e1f85cfe76882976cc83c7ed1d
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-06-09 17:10:39 -0600

    add different logger

--- tests/scripts/Logger.py
+++ tests/scripts/Logger.py
@@ -2,9 +2,15 @@ import logging
 import logging.handlers
 import logging.config
 import sys
+import os
+
+RESULT_LEVEL = 25
 
 class cLogger:
     def __init__(self,logLevel=None):
-        logging.config.fileConfig('/var/lib/stbt/test-pack/tests/scripts/logging.conf',disable_existing_loggers=False)
-        self.note = logging.getLogger(__name__)
+		logging.addLevelName(RESULT_LEVEL,"RESULT_LEVEL")
+		logging.config.fileConfig('/var/lib/stbt/test-pack/tests/scripts/logging.conf',disable_existing_loggers=False)
+		self.note = logging.getLogger(__name__)
+		self.note1 = logging.getLogger("resultLogger")
+
 Logger = cLogger(logLevel=logging.DEBUG)
