01dc01e49f9982dd71d89fcdc404afc647991e1e
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-06-09 14:27:08 -0600

    have variable

--- tests/scripts/KeywordDriver.py
+++ tests/scripts/KeywordDriver.py
@@ -450,15 +450,15 @@ class Execution:
             Logger.note.info("|%s|%s|%s|%s" %(eachTestCase,dicTCStatus[eachTestCase],dicTCCreatedTime[eachTestCase],dicTCElapsedTime[eachTestCase]))
 
 
+        LOG_FILENAME = 'logging_example.log'
         f = open(LOG_FILENAME, 'w')
 
         import logging
 
-        LOG_FILENAME = 'logging_example.log'
         logging.basicConfig(filename=LOG_FILENAME,
                             level=logging.DEBUG,
                             )
-        
+
         logging.debug('This is a debug message')
         logging.info('This is an info message')
         logging.warning('This is a warning message')
