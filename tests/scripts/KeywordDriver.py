0495693ffa6be2d1414a4a2863b48b79c60992ff
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-06-09 14:24:13 -0600

    try to debug

--- tests/scripts/KeywordDriver.py
+++ tests/scripts/KeywordDriver.py
@@ -450,21 +450,15 @@ class Execution:
             Logger.note.info("|%s|%s|%s|%s" %(eachTestCase,dicTCStatus[eachTestCase],dicTCCreatedTime[eachTestCase],dicTCElapsedTime[eachTestCase]))
 
 
+        f = open(LOG_FILENAME, 'w')
+
         import logging
-        import sys
 
-        LEVELS = { 'debug':logging.DEBUG,
-                    'info':logging.INFO,
-                    'warning':logging.WARNING,
-                    'error':logging.ERROR,
-                    'critical':logging.CRITICAL,
-                    }
-
-
-        level_name = 'info'
-        level = LEVELS.get(level_name, logging.NOTSET)
-        logging.basicConfig(level=level)
-
+        LOG_FILENAME = 'logging_example.log'
+        logging.basicConfig(filename=LOG_FILENAME,
+                            level=logging.DEBUG,
+                            )
+        
         logging.debug('This is a debug message')
         logging.info('This is an info message')
         logging.warning('This is a warning message')
