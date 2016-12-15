519d8ed44aca53a80122bd966cbb2e32e984de80
Vinoth Kumar Ravichandran <vinoth.ravichandran@echostar.com>
2016-12-15 12:51:40 -0700

    pointing to access popular search of aws-qa

--- tests/scripts/Constants.py
+++ tests/scripts/Constants.py
@@ -128,7 +128,7 @@ class Constants(object):
     KEY_PAGEUP = "KEY_PAGEUP"
 
     # List of URL(s)
-    POPULAR_SEARCH_URL = "http://tms-catalog-ext.dishanywhere.com:5228/get_popular_search/"
+    POPULAR_SEARCH_URL = "http://aws-qa-supair-admin.metacloud.media/get_popular_search/"
     #TMS_BASE_URL = "http://tms-catalog.dishanywhere.com:9200/"
     TMS_BASE_URL = "http://chy-prod-supair-admin.metacloud.media:9200/"
     NETFLIX_ORIGINALS_URL = "http://aws-qa-supair-admin.metacloud.media/api_netflix_originals_list/"
